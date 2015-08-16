04a4c95d4e691f5dd3eb56fc910f64c517bcc69d
Pascal Mayer <laxap4@gmail.com>
2015-08-16 02:42:42 +0200

    Support for TYPO3 7.4.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'typo3@bsdist.ch',
 	'author_company' => '',
 	'shy' => '',
-	'version' => '1.2.4',
+	'version' => '1.2.5',
 	'priority' => 'top',
 	'module' => '',
 	'state' => 'beta',
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'lockType' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.3.99',
+			'typo3' => '6.2.0-7.4.99',
 		),
 		'conflicts' => array(
 			'bootstrap_package' => '',
